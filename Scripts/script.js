f5d7c1a6ab2598977f54925855bbc6527873a557
Hendriuss <hendriussilva58@gmail.com>
2025-08-27 20:33:21 -0300

    criando botão

--- Scripts/script.js
+++ Scripts/script.js
@@ -2,3 +2,8 @@ console.log(document.body);
 
 const listarItens = document.getElementsByTagName('li');
 console.log(listarItens);
+
+const botao = document.getElementById('btn');
+botao.addEventListener('click', function() {
+    alert("você clicou no botão!")
+});
