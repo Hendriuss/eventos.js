5675ea11cd7820d1aa38d0c11da67362d0f3c6cd
Hendriuss <hendriussilva58@gmail.com>
2025-08-27 21:29:45 -0300

    alteração js

--- Scripts/script.js
+++ Scripts/script.js
@@ -3,7 +3,12 @@ console.log(document.body);
 const listarItens = document.getElementsByTagName('li');
 console.log(listarItens);
 
-const botao = document.getElementById('btn');
-botao.addEventListener('click', function() {
-    alert("você clicou no botão!")
-});
+// const botao = document.getElementById('btn');
+// botao.addEventListener('click', function() {
+   // alert("você clicou no botão!")
+// });
+
+const cor = document.getElementById('btn');
+cor.addEventListener('click', function(){
+    document.body.style.backgroundColor = 'green'
+})
